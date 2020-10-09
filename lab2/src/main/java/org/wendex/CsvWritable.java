08f7d7949b6a193f4329e5bd9fe2745bff12bec8
Wendex <2wendex2@mail.ru>
2020-10-09 22:43:31 +0300

    Scripted auto-commit on change (2020-10-09 22:43:30) by gitwatch.sh

--- lab2/src/main/java/org/wendex/CsvWritable.java
+++ lab2/src/main/java/org/wendex/CsvWritable.java
@@ -1,12 +1,9 @@
 package org.wendex;
 
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.fs.Path;
 
 public class CsvWritable implements Writable {
-	
+
 }
