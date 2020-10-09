4dfc8343a369d135bddb35b9b88df016fa593524
Wendex <2wendex2@mail.ru>
2020-10-09 23:04:30 +0300

    Scripted auto-commit on change (2020-10-09 23:04:30) by gitwatch.sh

--- lab2/src/main/java/org/wendex/CsvWritable.java
+++ lab2/src/main/java/org/wendex/CsvWritable.java
@@ -1,11 +1,28 @@
 package org.wendex;
 
 import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AirportWritable implements Writable {
 	private int id;
 	private String name;
 
-	
+	@Override
+	public void write(DataOutput dataOutput) throws IOException {
+		dataOutput.writeChars(Integer.toString(id));
+		dataOutput.writeChars(",\"");
+		dataOutput.writeChars(name.replaceAll("\"", "\"\""));
+		dataOutput.writeChar('"');
+	}
+
+	 @Override
+	 public void readFields(DataInput dataInput) throws IOException {
+		for (String s = dataInput.readLine(); s != null; s = dataInput.readLine()) {
+			
+		}
+	 }
 }
