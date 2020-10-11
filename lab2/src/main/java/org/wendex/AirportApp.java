30862a79386cc2a4f43b5dc937862bdcf6324c4e
Wendex <2wendex2@mail.ru>
2020-10-11 21:42:08 +0300

    Scripted auto-commit on change (2020-10-11 21:42:08) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportApp.java
+++ lab2/src/main/java/org/wendex/AirportApp.java
@@ -25,7 +25,7 @@ public class AirportApp {
 		job.setReducerClass(AirportReducer.class);
 		job.setOutputKeyClass(AirportWritableComparable.class);
 		job.setOutputValueClass(Text.class);
-		job.setNumReduceTasks(3);
+		job.setNumReduceTasks(2);
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
