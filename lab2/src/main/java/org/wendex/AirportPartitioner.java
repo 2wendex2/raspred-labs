9d50288f4affe3f009e50afceb19b7f8b96d6495
Wendex <2wendex2@mail.ru>
2020-10-11 21:55:13 +0300

    Scripted auto-commit on change (2020-10-11 21:55:13) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportPartitioner.java
+++ lab2/src/main/java/org/wendex/AirportPartitioner.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
     @Override
-    public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int i) {
+    public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int numReduceTasks) {
         return ;
     }
 }
