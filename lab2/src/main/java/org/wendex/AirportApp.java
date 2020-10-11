ec48d44d24d20b434858a9df4d70694abbafa517
Wendex <2wendex2@mail.ru>
2020-10-11 21:28:16 +0300

    Scripted auto-commit on change (2020-10-11 21:28:15) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportApp.java
+++ lab2/src/main/java/org/wendex/AirportApp.java
@@ -2,6 +2,8 @@ package org.wendex;
 
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,14 +12,14 @@ import org.apache.hadoop.fs.Path;
 public class AirportApp {
 	public static void main(String[] args) throws Exception {
 		if (args.length != 3) {
-			System.err.println("Usage: AirportApp <input path> <output path>");
+			System.err.println("Usage: AirportApp <airports input path> <flights input path> <output path>");
 			System.exit(-1);
 		}
 		Job job = Job.getInstance();
 		job.setJarByClass(AirportApp.class);
 		job.setJobName("Airport");
-		M
-		FileInputFormat.addInputPath(job, new Path(args[1]));
+		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
 		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		job.setMapperClass(FlightMapper.class);
 		job.setReducerClass(AirportReducer.class);
