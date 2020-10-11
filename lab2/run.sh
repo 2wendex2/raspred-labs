8dedc3c2565c9a7d759b11abb4b41e767111e0a0
Wendex <2wendex2@mail.ru>
2020-10-11 20:04:57 +0300

    Scripted auto-commit on change (2020-10-11 20:04:57) by gitwatch.sh

--- lab2/run.sh
+++ lab2/run.sh
@@ -3,7 +3,9 @@ rm -r output
 mvn package
 hadoop fs -rm -r output
 hadoop fs -rm -r L_AIRPORT_ID.csv
+hadoop fs -rm -r 664600583_T_ONTIME_sample.csv
 hadoop fs -copyFromLocal L_AIRPORT_ID.csv
+hadoop fs -copyFromLocal 664600583_T_ONTIME_sample.csv
 hadoop org.wendex.AirportApp L_AIRPORT_ID.csv output
 hadoop fs -copyToLocal output
 
