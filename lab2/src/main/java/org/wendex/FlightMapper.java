d0e0e38c992137f39ea9c9892c5beaa190db0ab6
Wendex <2wendex2@mail.ru>
2020-10-11 19:55:17 +0300

    Scripted auto-commit on change (2020-10-11 19:55:16) by gitwatch.sh

--- lab2/src/main/java/org/wendex/FlightMapper.java
+++ lab2/src/main/java/org/wendex/FlightMapper.java
@@ -1,4 +1,23 @@
 package org.wendex;
 
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        String[] arr = CsvTools.read(value.toString());
+        int id;
+        try {
+            id = Integer.parseInt(arr[0]);
+        } catch (NumberFormatException exception) {
+            return;
+        }
+
+        context.write(new AirportWritableComparable(id, false), new Text(arr[1]));
+    }
 }
