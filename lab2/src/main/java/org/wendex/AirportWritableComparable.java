4ae18d4d757bf22f66ea28f09e89f65da1823410
Wendex <2wendex2@mail.ru>
2020-10-11 17:47:57 +0300

    Scripted auto-commit on change (2020-10-11 17:47:57) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportWritableComparable.java
+++ lab2/src/main/java/org/wendex/AirportWritableComparable.java
@@ -2,6 +2,25 @@ package org.wendex;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportWritableComparable implements WritableComparable<AirportWritableComparable> {
-    
+    int id;
+    boolean flightFlag;
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(id);
+        dataOutput.writeChar(',');
+        dataOutput.writeBoolean(flightFlag);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        id = dataInput.readInt();
+        dataInput.readChar();
+        flightFlag = dataInput.readBoolean();
+    }
 }
