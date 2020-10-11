8bf8f195ecc9cce11682f45909ce4a781f76ac03
Wendex <2wendex2@mail.ru>
2020-10-11 22:17:04 +0300

    Scripted auto-commit on change (2020-10-11 22:17:04) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportComparator.java
+++ lab2/src/main/java/org/wendex/AirportComparator.java
@@ -1,7 +1,11 @@
 package org.wendex;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
-    
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        return ((AirportWritableComparable)a).getId() - ((AirportWritableComparable)b).getId();
+    }
 }
