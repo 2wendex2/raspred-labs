8ba979529f3971cf0570a794e4bf26a3348f1570
Wendex <2wendex2@mail.ru>
2020-10-11 16:36:52 +0300

    Scripted auto-commit on change (2020-10-11 16:36:52) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportApp.java
+++ lab2/src/main/java/org/wendex/AirportApp.java
@@ -7,19 +7,19 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.fs.Path;
 
-public class WordCountApp {
+public class AirportApp {
 	public static void main(String[] args) throws Exception {
 		if (args.length != 2) {
-			System.err.println("Usage: WordCountApp <input path> <output path>");
+			System.err.println("Usage: AirportApp <input path> <output path>");
 			System.exit(-1);
 		}
 		Job job = Job.getInstance();
-		job.setJarByClass(WordCountApp.class);
-		job.setJobName("Word count");
+		job.setJarByClass(AirportApp.class);
+		job.setJobName("Airport");
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		job.setMapperClass(WordMapper.class);
-		job.setReducerClass(WordReducer.class);
+		job.setMapperClass(AirportMapper.class);
+		job.setReducerClass(AirportReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		job.setNumReduceTasks(2);
