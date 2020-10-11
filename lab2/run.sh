bebf66a29c6472006b17ad5128df485f9c2e7905
Wendex <2wendex2@mail.ru>
2020-10-11 20:05:15 +0300

    Scripted auto-commit on change (2020-10-11 20:05:15) by gitwatch.sh

--- lab2/run.sh
+++ lab2/run.sh
@@ -6,6 +6,6 @@ hadoop fs -rm -r L_AIRPORT_ID.csv
 hadoop fs -rm -r 664600583_T_ONTIME_sample.csv
 hadoop fs -copyFromLocal L_AIRPORT_ID.csv
 hadoop fs -copyFromLocal 664600583_T_ONTIME_sample.csv
-hadoop org.wendex.AirportApp L_AIRPORT_ID.csv output
+hadoop org.wendex.AirportApp L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output
 hadoop fs -copyToLocal output
 
