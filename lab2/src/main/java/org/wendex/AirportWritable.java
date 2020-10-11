9c28a5745007d6a330595c4e478a05c6fcc2e512
Wendex <2wendex2@mail.ru>
2020-10-11 17:33:16 +0300

    Scripted auto-commit on change (2020-10-11 17:33:15) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportWritable.java
+++ lab2/src/main/java/org/wendex/AirportWritable.java
@@ -5,7 +5,6 @@ import org.apache.hadoop.io.Writable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class AirportWritable implements Writable {
 	private int id;
