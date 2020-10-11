1c75289bddd8c206dc94258dba28071db0fa4661
Wendex <2wendex2@mail.ru>
2020-10-11 18:22:10 +0300

    Scripted auto-commit on change (2020-10-11 18:22:10) by gitwatch.sh

--- lab2/run.sh
+++ lab2/run.sh
@@ -1,9 +1,9 @@
-export HADOOP_CLASSPATH=target/lab1-1.0-SNAPSHOT.jar
+export HADOOP_CLASSPATH=target/lab2-1.0-SNAPSHOT.jar
 rm -r output
 mvn package
 hadoop fs -rm -r output
-hadoop fs -rm -r war.txt
-hadoop fs -copyFromLocal war.txt
-hadoop org.wendex.WordCountApp war.txt output
+hadoop fs -rm -r L_AIRPORT_ID.csv
+hadoop fs -copyFromLocal L_AIRPORT_ID.csv
+hadoop org.wendex.AirportApp L_AIRPORT_ID.csv output
 hadoop fs -copyToLocal output
 
