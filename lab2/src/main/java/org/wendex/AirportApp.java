a57e5eeb0d42824ba0425ff9e1efdcf91571ef84
Wendex <2wendex2@mail.ru>
2020-10-11 22:02:09 +0300

    Scripted auto-commit on change (2020-10-11 22:02:09) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportApp.java
+++ lab2/src/main/java/org/wendex/AirportApp.java
@@ -1,11 +1,9 @@
 package org.wendex;
 
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.fs.Path;
 
