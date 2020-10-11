7c206fbbb04de21421cd6cacbf17457aacd1ad06
Wendex <2wendex2@mail.ru>
2020-10-11 20:05:26 +0300

    Scripted auto-commit on change (2020-10-11 20:05:26) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportApp.java
+++ lab2/src/main/java/org/wendex/AirportApp.java
@@ -16,7 +16,7 @@ public class AirportApp {
 		Job job = Job.getInstance();
 		job.setJarByClass(AirportApp.class);
 		job.setJobName("Airport");
-		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileInputFormat.addInputPath(job, new Path(args[]));
 		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		job.setMapperClass(FlightMapper.class);
 		job.setReducerClass(AirportReducer.class);
