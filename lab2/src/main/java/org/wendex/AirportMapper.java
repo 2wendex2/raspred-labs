9792c9d6429ccc6cb82e4830695976ae51d7aad7
Wendex <2wendex2@mail.ru>
2020-10-11 18:29:00 +0300

    Scripted auto-commit on change (2020-10-11 18:29:00) by gitwatch.sh

--- lab2/src/main/java/org/wendex/AirportMapper.java
+++ lab2/src/main/java/org/wendex/AirportMapper.java
@@ -11,6 +11,8 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
 	protected void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
 		AirportWritable airportWritable = new AirportWritable(value);
-		context.write(airportWritable.toAirportWritableComparable(), airportWritable);
+		if (airportWritable.isValid()) {
+			context.write(airportWritable.toAirportWritableComparable(), airportWritable);
+		}
 	}
 }
