d600e6844c88a74d5622322008d3515143bdc9b2
Wendex <2wendex2@mail.ru>
2020-10-11 19:27:42 +0300

    Scripted auto-commit on change (2020-10-11 19:27:42) by gitwatch.sh

--- lab2/src/main/java/org/wendex/CsvTools.java
+++ lab2/src/main/java/org/wendex/CsvTools.java
@@ -1,4 +1,14 @@
 package org.wendex;
 
 public class CsvTools {
+    public static String[] read(String s) {
+        String[] arr = s.replaceAll("\"\"", "\"").split(",");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].charAt(0) == '\"') {
+                arr[i] = arr[i].substring(1, arr[i].length() - 1);
+            }
+        }
+    }
+
+    public static write
 }
