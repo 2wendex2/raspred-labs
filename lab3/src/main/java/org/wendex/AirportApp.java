24440e3412fcd01ab42a686db81309a9127d672e
Wendex <2wendex2@mail.ru>
2020-11-19 23:25:18 +0300

    Scripted auto-commit on change (2020-11-19 23:25:17) by gitwatch.sh

--- lab3/src/main/java/org/wendex/AirportApp.java
+++ lab3/src/main/java/org/wendex/AirportApp.java
@@ -1,7 +1,11 @@
 package org.wendex;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class AirportApp {
     public static void main(String[] args) {
-        
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
