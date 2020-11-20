bae2b4d4e090074e793310fad891d8e7b543250b
Wendex <2wendex2@mail.ru>
2020-11-20 18:24:37 +0300

    Scripted auto-commit on change (2020-11-20 18:24:37) by gitwatch.sh

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,5 +1,5 @@
 export HADOOP_CLASSPATH=target/lab3-1.0-SNAPSHOT.jar
-rm output
+rm -r output
 hadoop fs -rm -r output
 mvn package
 spark-submit --class org.wendex.AirportApp --master yarn-client --num-executors 3 target/lab3-1.0-SNAPSHOT.jar
