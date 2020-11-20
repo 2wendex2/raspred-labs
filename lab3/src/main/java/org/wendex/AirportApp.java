bfca2be330386e96bab62237d668eb4e079e1fca
Wendex <2wendex2@mail.ru>
2020-11-20 18:32:45 +0300

    Scripted auto-commit on change (2020-11-20 18:32:45) by gitwatch.sh

--- lab3/src/main/java/org/wendex/AirportApp.java
+++ lab3/src/main/java/org/wendex/AirportApp.java
@@ -66,7 +66,7 @@ public class AirportApp {
                     Map<Integer, String> map = airportsBroadcasted.value();
                     return t._1._1.toString() + "\t" + map.get(t._1._1) + "\t\t\t" +
                             t._1._2.toString() + "\t" + map.get(t._1._2) + "\t\t\t" +
-                            t._2.getMaxDelay() + "\t\t" + t._2.getPercent();
+                            t._2.getMaxDelay() + "\t\t" + t._2.getPercent() + "%";
                 }).saveAsTextFile("output");
 
     }
