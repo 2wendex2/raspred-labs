f35149076060d3486906b2b5686a26b7198b591f
Wendex <2wendex2@mail.ru>
2020-11-20 14:25:54 +0300

    Scripted auto-commit on change (2020-11-20 14:25:54) by gitwatch.sh

--- lab3/src/main/java/org/wendex/FlightData.java
+++ lab3/src/main/java/org/wendex/FlightData.java
@@ -3,5 +3,19 @@ package org.wendex;
 import java.io.Serializable;
 
 public class FlightData implements Serializable {
-    
+    private int flightCount = 0;
+    private int cancelledDelayCount = 0;
+    private double maxDelay = Double.NEGATIVE_INFINITY;
+
+    public FlightData() {}
+
+    public void addFlight(boolean cancelled, double delay) {
+        if (delay > maxDelay) {
+            maxDelay = delay;
+        }
+        flightCount++;
+        if (cancelled || delay > 0) {
+            cancelledDelayCount++;
+        }
+    }
 }
