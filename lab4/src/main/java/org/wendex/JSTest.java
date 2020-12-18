d03b7c6f8afd9e1eceefb7a3ab39f5a3cd9b3ea4
Wendex <2wendex2@mail.ru>
2020-12-18 04:44:28 +0300

    Scripted auto-commit on change (2020-12-18 04:44:21) by gitwatch.sh

--- lab4/src/main/java/org/wendex/JSTest.java
+++ lab4/src/main/java/org/wendex/JSTest.java
@@ -1,7 +1,10 @@
 import akka.actor.ActorSystem;
 
+import javax.script.ScriptEngine;
+
 public class JSTest {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("test");
+        ScriptEngine eng;
     }
 }
