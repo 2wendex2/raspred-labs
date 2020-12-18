cf0952a583e26279c18e608c0bb2cc90842c8031
Wendex <2wendex2@mail.ru>
2020-12-18 23:32:09 +0300

    Scripted auto-commit on change (2020-12-18 23:32:09) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRouterActor.java
+++ lab4/src/main/java/org/wendex/TestRouterActor.java
@@ -11,10 +11,10 @@ public class TestRouterActor extends AbstractActor {
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestRunMessage.class, m -> {
-                    ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("p");
+                    /*ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("p");
                     scriptEngine.eval(m.getJsString());
                     Invocable invocable = (Invocable)scriptEngine;
-                    System.out.println(invocable.invokeFunction(m.getFunctionName(), m.getParams()));
+                    System.out.println(invocable.invokeFunction(m.getFunctionName(), m.getParams()));*/
                 }).build();
     }
 }
