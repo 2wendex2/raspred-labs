3acc429066281c6e9d72fd4119aa8e6b6bfbeeb9
Wendex <2wendex2@mail.ru>
2020-12-18 05:28:48 +0300

    Scripted auto-commit on change (2020-12-18 05:28:48) by gitwatch.sh

--- lab4/src/main/java/org/wendex/PackageTestsMessages.java
+++ lab4/src/main/java/org/wendex/PackageTestsMessages.java
@@ -1,5 +1,15 @@
 package org.wendex;
 
+import java.util.HashMap;
+
 public class PackageTestsMessages {
-    
+    private HashMap<String, Object> testsResults;
+
+    public HashMap<String, Object> getTestsResults() {
+        return testsResults;
+    }
+
+    public PackageTestsMessages(HashMap<String, Object> testsResults) {
+        this.testsResults = testsResults;
+    }
 }
