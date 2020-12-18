36a10df427840722192c03b4e7d5b1195e071df3
Wendex <2wendex2@mail.ru>
2020-12-18 21:57:13 +0300

    Scripted auto-commit on change (2020-12-18 21:57:13) by gitwatch.sh

---
+++ lab4/src/main/java/org/wendex/TestRunMessage.java
@@ -0,0 +1,25 @@
+package org.wendex;
+
+public class TestRunMessage {
+    private int packageId;
+    private String functionName;
+    private String jsString;
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getJsString() {
+        return jsString;
+    }
+
+    public TestRunMessage(int packageId, String functionName, String jsString) {
+        this.packageId = packageId;
+        this.functionName = functionName;
+        this.jsString = jsString;
+    }
+}
