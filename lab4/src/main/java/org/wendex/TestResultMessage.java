4f0615a3c5256f3493752a0ba8a8c6cb1d5110d8
Wendex <2wendex2@mail.ru>
2020-12-18 05:17:21 +0300

    Scripted auto-commit on change (2020-12-18 05:17:19) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestResultMessage.java
+++ lab4/src/main/java/org/wendex/TestResultMessage.java
@@ -1,4 +1,25 @@
 package org.wendex;
 
 public class TestResultMessage {
+    private int packageId;
+    private String testName;
+    private Object testResult;
+
+    public TestResultMessage(int packageId, String testName, Object testResult) {
+        this.packageId = packageId;
+        this.testName = testName;
+        this.testResult = testResult;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public Object getTestResult() {
+        return testResult;
+    }
 }
