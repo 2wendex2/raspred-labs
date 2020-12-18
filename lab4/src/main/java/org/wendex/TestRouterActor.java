1a1dbcc830956b2df03bb02af9c1f59ee3cca139
Wendex <2wendex2@mail.ru>
2020-12-19 00:26:54 +0300

    Scripted auto-commit on change (2020-12-19 00:26:53) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRouterActor.java
+++ lab4/src/main/java/org/wendex/TestRouterActor.java
@@ -9,7 +9,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class TestRouterActor extends AbstractActor {
-    private ActorRef storageActor = system.a
+    private ActorRef storageActor = getContext().
 
     public TestRouterActor() {
 
