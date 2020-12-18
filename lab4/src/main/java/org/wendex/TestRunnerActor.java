dca3c98d437a2153d946f2e75799c5160e6b5957
Wendex <2wendex2@mail.ru>
2020-12-18 21:58:24 +0300

    Scripted auto-commit on change (2020-12-18 21:58:24) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRunnerActor.java
+++ lab4/src/main/java/org/wendex/TestRunnerActor.java
@@ -3,13 +3,14 @@ package org.wendex;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.ScriptEngine;
 import java.util.HashMap;
 
 public class TestRunnerActor {
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestRunMessage.class, m -> {
-                    
+                    ScriptEngine scriptEngine 
                 }).build();
     }
 }
