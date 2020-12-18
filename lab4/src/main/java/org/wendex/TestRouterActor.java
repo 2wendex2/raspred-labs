86cd2ffe7995a1379b0b2178a5489ecd66e612ea
Wendex <2wendex2@mail.ru>
2020-12-19 01:10:52 +0300

    Scripted auto-commit on change (2020-12-19 01:10:52) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRouterActor.java
+++ lab4/src/main/java/org/wendex/TestRouterActor.java
@@ -11,18 +11,18 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class TestRouterActor extends AbstractActor {
-    private ActorRef storageActor = getContext().actorOf(Props.create(StorageActor.class));
-    //private ActorRef testRunnerPool = getContext().actorOf(new RoundRobinPool(5)
-      //      .props(Props.create(TestRouterActor.class)));
+    //private ActorRef storageActor = getContext().actorOf(Props.create(StorageActor.class));
+    private ActorRef testRunnerPool = getContext().actorOf(new RoundRobinPool(1)
+            .props(Props.create(TestRouterActor.class)));
 
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestRunMessage.class, m -> {
                     //testRunnerPool.tell(m, self());
                 }).match(TestQueryMessage.class, m -> {
-                    storageActor.tell(m, self());
+                    //storageActor.tell(m, self());
                 }).match(PackageTestsMessage.class, m -> {
-                    storageActor.tell(m, self());
+                    //storageActor.tell(m, self());
                 }).build();
     }
 }
