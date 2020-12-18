f32415efcce1603c108eed804865369e3cc7efe4
Wendex <2wendex2@mail.ru>
2020-12-18 22:37:37 +0300

    Scripted auto-commit on change (2020-12-18 22:37:37) by gitwatch.sh

--- lab4/src/main/java/org/wendex/JSTest.java
+++ lab4/src/main/java/org/wendex/JSTest.java
@@ -6,6 +6,6 @@ import javax.script.ScriptEngine;
 public class JSTest {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("test");
-        ActorRef storageActor
+        ActorRef storageActor = 
     }
 }
