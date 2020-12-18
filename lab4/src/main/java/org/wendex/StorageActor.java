04b001e48ecbc734919088b265c76843d723b9ed
Wendex <2wendex2@mail.ru>
2020-12-18 05:12:22 +0300

    Scripted auto-commit on change (2020-12-18 05:12:22) by gitwatch.sh

--- lab4/src/main/java/org/wendex/StorageActor.java
+++ lab4/src/main/java/org/wendex/StorageActor.java
@@ -1,8 +1,10 @@
 package org.wendex;
 
+import akka.actor.AbstractActor;
+
 import java.util.HashMap;
 
-public class StorageActor {
+public class StorageActor extends AbstractActor {
     private HashMap<Integer, HashMap<String, Object>> store;
 
     
