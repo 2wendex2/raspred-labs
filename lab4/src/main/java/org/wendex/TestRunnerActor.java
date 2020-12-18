ad3c16851c1488a439f9ffd785b20cb00ec4eafa
Wendex <2wendex2@mail.ru>
2020-12-18 23:48:12 +0300

    Scripted auto-commit on change (2020-12-18 23:48:10) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRunnerActor.java
+++ lab4/src/main/java/org/wendex/TestRunnerActor.java
@@ -18,7 +18,7 @@ public class TestRunnerActor extends  AbstractActor{
                     scriptEngine.eval(m.getJsString());
                     Invocable invocable = (Invocable)scriptEngine;
                     Object o = invocable.invokeFunction(m.getFunctionName(), m.getParams());
-                    throw RuntimeException(o.class.getName());
+                    throw new RuntimeException(o.getClass().getName());
                 }).build();
     }
 }
