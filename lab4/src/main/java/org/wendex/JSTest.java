2774edffcdb18c2c711885ae2464414801b691aa
Wendex <2wendex2@mail.ru>
2020-12-18 22:38:12 +0300

    Scripted auto-commit on change (2020-12-18 22:38:11) by gitwatch.sh

--- lab4/src/main/java/org/wendex/JSTest.java
+++ lab4/src/main/java/org/wendex/JSTest.java
@@ -1,11 +1,14 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import org.wendex.TestRouterActor;
 
 import javax.script.ScriptEngine;
 
 public class JSTest {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("test");
-        ActorRef storageActor = actorSystem.actorOf()
+        ActorRef storageActor = actorSystem.actorOf(Props.create(TestRouterActor.class));
+        
     }
 }
