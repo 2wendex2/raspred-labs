4592338159969e842c76d1e707839f4dbe4a6ef4
Wendex <2wendex2@mail.ru>
2020-12-18 22:53:01 +0300

    Scripted auto-commit on change (2020-12-18 22:53:01) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRouterActor.java
+++ lab4/src/main/java/org/wendex/TestRouterActor.java
@@ -1,7 +1,20 @@
 package org.wendex;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 public class TestRouterActor extends AbstractActor {
-
+    public AbstractActor.Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(TestRunMessage.class, m -> {
+                    ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("p");
+                    scriptEngine.eval(m.getJsString());
+                    Invocable invocable = (Invocable)scriptEngine;
+                    System.out.println(invocable.invokeFunction(m.getFunctionName(), m.getParams()));
+                }).build();
+    }
 }
