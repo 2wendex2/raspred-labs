8c0f52c731a1766f0a97834c1085ff895146691e
Wendex <2wendex2@mail.ru>
2020-12-18 05:24:46 +0300

    Scripted auto-commit on change (2020-12-18 05:24:46) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestQueryMessage.java
+++ lab4/src/main/java/org/wendex/TestQueryMessage.java
@@ -1,4 +1,13 @@
 package org.wendex;
 
 public class TestQueryMessage {
+    private int packageId;
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public TestQueryMessage(int packageId) {
+        this.packageId = packageId;
+    }
 }
