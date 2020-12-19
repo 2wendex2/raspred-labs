a31d306957f23d475a86b5faf8a4d5bfa40448d6
Wendex <2wendex2@mail.ru>
2020-12-19 20:07:35 +0300

    Scripted auto-commit on change (2020-12-19 20:07:35) by gitwatch.sh

--- lab4/src/main/java/org/wendex/HttpQuery.java
+++ lab4/src/main/java/org/wendex/HttpQuery.java
@@ -1,4 +1,11 @@
 package org.wendex;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class HttpQuery {
+    @JsonProperty("packageId")          private int packageId;
+    @JsonProperty("functionName")       private String functionName;
+    @JsonProperty("jsScript")           private String jsScript;
+    @JsonProperty("params")             private Object[] params;
+    @JsonProperty("testName")           private String testName;
 }
