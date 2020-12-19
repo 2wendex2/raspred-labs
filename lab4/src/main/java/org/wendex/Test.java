865ac7cc99f764e5bbd04c4c7491cbff8b5f5fa2
Wendex <2wendex2@mail.ru>
2020-12-19 20:25:18 +0300

    Scripted auto-commit on change (2020-12-19 20:25:18) by gitwatch.sh

--- lab4/src/main/java/org/wendex/Test.java
+++ lab4/src/main/java/org/wendex/Test.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Test {
-    static final String PROPERTY_TEST_NAME =
+    private static final String PROPERTY_TEST_NAME = "testName";
+    private static final String PROPERTY_PARAMS = "params";
+    private static final String PROPERTY_EXPECTED_RESULT = "expectedResult";
 
-    @JsonProperty("testName")           private String testName;
-    @JsonProperty("params")             private Object[] params;
-    @JsonProperty("expectedResult")     private Object expectedResult;
+    @JsonProperty(PROPERTY_TEST_NAME)           private String testName;
+    @JsonProperty(PROPERTY_PARAMS)              private Object[] params;
+    @JsonProperty(PROPERTY_EXPECTED_RESULT)     private Object expectedResult;
 
     public String getTestName() {
         return testName;
@@ -22,9 +24,9 @@ public class Test {
         return params;
     }
 
-    @JsonCreator public Test(@JsonProperty("testName")          String testName,
-                             @JsonProperty("params")            Object[] params,
-                             @JsonProperty("expectedResult")    Object expectedResult) {
+    @JsonCreator public Test(@JsonProperty(PROPERTY_TEST_NAME)          String testName,
+                             @JsonProperty(PROPERTY_PARAMS)             Object[] params,
+                             @JsonProperty(PROPERTY_EXPECTED_RESULT)    Object expectedResult) {
         this.testName = testName;
         this.params = params;
         this.expectedResult = expectedResult;
