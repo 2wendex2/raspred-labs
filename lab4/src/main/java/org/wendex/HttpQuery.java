fa853ad86a923e4731ce551c4e6a83180d1101d2
Wendex <2wendex2@mail.ru>
2020-12-19 20:34:46 +0300

    Scripted auto-commit on change (2020-12-19 20:34:46) by gitwatch.sh

--- lab4/src/main/java/org/wendex/HttpQuery.java
+++ lab4/src/main/java/org/wendex/HttpQuery.java
@@ -1,11 +1,42 @@
 package org.wendex;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class HttpQuery {
-    @JsonProperty("packageId")          private int packageId;
-    @JsonProperty("functionName")       private String functionName;
-    @JsonProperty("jsScript")           private String jsScript;
-    @JsonProperty("tests")              private Test[] tests;
+    private static final String PROPERTY_PACKAGE_ID = "packageId";
+    private static final String PROPERTY_FUNCTION_NAME = "functionName";
+    private static final String PROPERTY_JS_SCRIPT = "jsScript";
+    private static final String PROPERTY_TESTS = "tests";
 
+    @JsonProperty(PROPERTY_PACKAGE_ID)          private int packageId;
+    @JsonProperty(PROPERTY_FUNCTION_NAME)       private String functionName;
+    @JsonProperty(PROPERTY_JS_SCRIPT)           private String jsScript;
+    @JsonProperty(PROPERTY_TESTS)               private Test[] tests;
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public static String getPropertyFunctionName() {
+        return PROPERTY_FUNCTION_NAME;
+    }
+
+    @JsonCreator public HttpQuery(@JsonProperty(PROPERTY_PACKAGE_ID)          int packageId,
+                                  @JsonProperty(PROPERTY_FUNCTION_NAME)       String functionName,
+                                  @JsonProperty(PROPERTY_JS_SCRIPT)           String jsScript,
+                                  @JsonProperty(PROPERTY_TESTS)               Test[] tests) {
+        this.packageId = packageId;
+        this.functionName = functionName;
+        this.jsScript = jsScript;
+        this.tests = tests;
+    }
 }
