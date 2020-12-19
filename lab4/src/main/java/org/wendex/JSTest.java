51a070c6709ec46fdddbe8e6b8a0b426db388e64
Wendex <2wendex2@mail.ru>
2020-12-19 18:49:33 +0300

    Scripted auto-commit on change (2020-12-19 18:49:24) by gitwatch.sh

--- lab4/src/main/java/org/wendex/JSTest.java
+++ lab4/src/main/java/org/wendex/JSTest.java
@@ -6,7 +6,7 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import org.wendex.TestRouterActor;
 import org.wendex.TestRunMessage;
-import akka.http.scaladsl.Htt;
+import akka.http.javadsl.Htt;
 
 import javax.script.ScriptEngine;
 
