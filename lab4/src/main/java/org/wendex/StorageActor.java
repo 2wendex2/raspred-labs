0e259005c0c75040fee0284a1b3e07cc4e73e45c
Wendex <2wendex2@mail.ru>
2020-12-20 01:13:43 +0300

    Scripted auto-commit on change (2020-12-20 01:13:43) by gitwatch.sh

--- lab4/src/main/java/org/wendex/StorageActor.java
+++ lab4/src/main/java/org/wendex/StorageActor.java
@@ -6,7 +6,7 @@ import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
 
 public class StorageActor extends AbstractActor {
-    private HashMap<Integer, HashMap<String, Boolean>> store;
+    private HashMap<Integer, HashMap<String, Boolean>> store = new HashMap<>();
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
@@ -15,8 +15,6 @@ public class StorageActor extends AbstractActor {
                     h.put(m.getTestName(), m.getTestResult());
                 }).match(TestQueryMessage.class, m -> {
                     HashMap<String, Boolean> h = store.get(m.getPackageId());
-                    if (h == null)
-                        throw new TestException("Package with specified id not found");
                     sender().tell(new PackageTestsMessage(h), self());
                 }).build();
     }
