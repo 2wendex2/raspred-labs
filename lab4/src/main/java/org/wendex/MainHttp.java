cc6b7d11c889eccb818669ade6d71245b30a9681
Wendex <2wendex2@mail.ru>
2020-12-19 20:42:19 +0300

    Scripted auto-commit on change (2020-12-19 20:42:19) by gitwatch.sh

--- lab4/src/main/java/org/wendex/MainHttp.java
+++ lab4/src/main/java/org/wendex/MainHttp.java
@@ -1,10 +1,11 @@
 package org.wendex;
 
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.server.Route;
 import static akka.http.javadsl.server.Directives.*;
 
 public class MainHttp {
     public Route getRoute() {
-        return post(()-> entity());
+        return post(()-> entity(Jackson.unmarshaller()));
     }
 }
