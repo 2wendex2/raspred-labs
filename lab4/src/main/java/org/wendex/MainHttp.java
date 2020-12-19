da1cf3838e59b4c107e8c2b7ea1f5215ebd5477e
Wendex <2wendex2@mail.ru>
2020-12-19 22:53:27 +0300

    Scripted auto-commit on change (2020-12-19 22:53:27) by gitwatch.sh

--- lab4/src/main/java/org/wendex/MainHttp.java
+++ lab4/src/main/java/org/wendex/MainHttp.java
@@ -21,15 +21,8 @@ public class MainHttp {
             }
             return complete("SUCCESS");
         })).orElse(get(() -> parameter(PROPERTY_PACKAGE_ID, m -> {
-            Patterns.ask(actor, new TestQueryMessage(Integer.parseInt(m)), QUERY_TIMEOUT)
-                    .onComplete(new OnComplete<Object>() {
-                        @Override
-                        public void onComplete(Throwable failure, Object success) throws Throwable {
-
-                        }
-                    }, actorSystem.getDispatcher());
             return completeOKWithFuture(Patterns.ask(actor, new TestQueryMessage(Integer.parseInt(m)), QUERY_TIMEOUT),
-                    )
+                    Jackson.marshaller());
         })));
     }
 }
