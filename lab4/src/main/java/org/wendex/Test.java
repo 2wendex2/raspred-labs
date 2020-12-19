5ebe2f72b01764d57dc8fb73fd76e2e2cd7495f5
Wendex <2wendex2@mail.ru>
2020-12-19 20:11:15 +0300

    Scripted auto-commit on change (2020-12-19 20:11:15) by gitwatch.sh

--- lab4/src/main/java/org/wendex/Test.java
+++ lab4/src/main/java/org/wendex/Test.java
@@ -5,5 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Test {
     @JsonProperty("testName")           private String testName;
     @JsonProperty("params")             private Object[] params;
-    @JsonProperty("expectedResult")     private Test[] tests;
+    @JsonProperty("expectedResult")     private Object expectedResult;
+
+    
 }
