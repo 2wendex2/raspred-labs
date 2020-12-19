e1e153675332a07008f1bd00136bd00f8cbfdb98
Wendex <2wendex2@mail.ru>
2020-12-20 01:25:52 +0300

    Scripted auto-commit on change (2020-12-20 01:25:51) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRouterActor.java
+++ lab4/src/main/java/org/wendex/TestRouterActor.java
@@ -24,7 +24,7 @@ public class TestRouterActor extends AbstractActor {
                 }).match(TestQueryMessage.class, m -> {
                     storageActor.tell(m, self());
                 }).match(PackageTestsMessage.class, m -> {
-                    getSender().tell(m, self());
+                    sender().tell(m, self());
                 }).match(TestResultMessage.class, m -> {
                     storageActor.tell(m, self());
                 }).build();
