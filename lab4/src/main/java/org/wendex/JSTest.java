8e3fb3c03e1c628e5bc7b2f151a85dc880d176c7
Wendex <2wendex2@mail.ru>
2020-12-19 18:28:52 +0300

    Scripted auto-commit on change (2020-12-19 18:28:48) by gitwatch.sh

--- lab4/src/main/java/org/wendex/JSTest.java
+++ lab4/src/main/java/org/wendex/JSTest.java
@@ -12,6 +12,16 @@ import javax.script.ScriptEngine;
 public class JSTest {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("test");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
         ActorRef testRouterActor = actorSystem.actorOf(Props.create(TestRouterActor.class));
         testRouterActor
     }
