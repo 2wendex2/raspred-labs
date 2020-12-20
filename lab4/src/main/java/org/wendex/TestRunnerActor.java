17cf054bbae2af22906de42ff2b0b47acf176e9d
Wendex <2wendex2@mail.ru>
2020-12-20 04:08:17 +0300

    Scripted auto-commit on change (2020-12-20 04:08:17) by gitwatch.sh

--- lab4/src/main/java/org/wendex/TestRunnerActor.java
+++ lab4/src/main/java/org/wendex/TestRunnerActor.java
@@ -13,12 +13,12 @@ public class TestRunnerActor extends  AbstractActor{
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestRunMessage.class, m -> {
+                    System.out.println("pun");
                     ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName(JS_ENGINE_NAME);
                     scriptEngine.eval(m.getJsString());
                     Invocable invocable = (Invocable)scriptEngine;
                     Object o = invocable.invokeFunction(m.getFunctionName(), m.getParams());
                     sender().tell(m.toTestResultMessage(o), self());
-                    System.out.println("pun");
                 }).build();
     }
 }
