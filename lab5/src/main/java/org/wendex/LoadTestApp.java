aabb14a6b86039e88437d6c2865fa59e0c35c836
Wendex <2wendex2@mail.ru>
2020-12-20 22:49:06 +0300

    Scripted auto-commit on change (2020-12-20 22:49:06) by gitwatch.sh

--- lab5/src/main/java/org/wendex/LoadTestApp.java
+++ lab5/src/main/java/org/wendex/LoadTestApp.java
@@ -50,7 +50,7 @@ public class LoadTestApp {
                         if (r.getTime() != null)
                             return CompletableFuture.completedFuture(r.getTime());
 
-                        Sink<>
+                        Sink<Long, >
 
                         Sink<QueryMessage, CompletionStage<Long>> testSink = Flow
                                 .<QueryMessage>create()
