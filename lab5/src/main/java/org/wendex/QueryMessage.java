6ff9d3a12d40f1e472ff7b307ab1f906c705051c
Wendex <2wendex2@mail.ru>
2020-12-20 19:44:59 +0300

    Scripted auto-commit on change (2020-12-20 19:44:59) by gitwatch.sh

--- lab5/src/main/java/org/wendex/QueryMessage.java
+++ lab5/src/main/java/org/wendex/QueryMessage.java
@@ -1,5 +1,20 @@
 package org.wendex;
 
 public class QueryMessage {
-    String 
+    private String testUrl;
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getTestUrl() {
+        return testUrl;
+    }
+
+    public QueryMessage(String testUrl, int count) {
+        this.testUrl = testUrl;
+        this.count = count;
+    }
+
 }
