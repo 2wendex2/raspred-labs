80da002103b3409441e775813b8ded9f3518fbd9
Wendex <2wendex2@mail.ru>
2020-12-20 19:23:59 +0300

    Scripted auto-commit on change (2020-12-20 19:23:59) by gitwatch.sh

--- lab5/src/main/java/org/wendex/StoreActor.java
+++ lab5/src/main/java/org/wendex/StoreActor.java
@@ -3,5 +3,8 @@ package org.wendex;
 import akka.actor.AbstractActor;
 
 public class StoreActor extends AbstractActor {
-    
+    @Override
+    public Receive createReceive() {
+        return 
+    }
 }
