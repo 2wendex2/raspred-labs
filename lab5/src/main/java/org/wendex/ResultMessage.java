c111b4fe9cfb9aede551f545d16013a0fd053442
Wendex <2wendex2@mail.ru>
2020-12-20 19:58:34 +0300

    Scripted auto-commit on change (2020-12-20 19:58:34) by gitwatch.sh

--- lab5/src/main/java/org/wendex/ResultMessage.java
+++ lab5/src/main/java/org/wendex/ResultMessage.java
@@ -1,14 +1,34 @@
 package org.wendex;
 
+import akka.japi.Pair;
+
 public class ResultMessage {
     private Long time;
-
+    private Pair<String, Integer> p;
 
     public Long getTime() {
         return time;
     }
 
-    public ResultMessage(Long time) {
+    public Pair<String, Integer> getPair() {
+        return p;
+    }
+
+    public String getTestUrl() {
+        return p.first();
+    }
+
+    public int getCount() {
+        return p.second();
+    }
+
+    public ResultMessage(Long time, Pair<String, Integer> p) {
+        this.time = time;
+        this.p = p;
+    }
+
+    public ResultMessage(Long time, String testUrl, int count) {
+        p = new Pair<>(testUrl, count);
         this.time = time;
     }
 }
--- lab5/src/main/java/org/wendex/StoreActor.java
+++ lab5/src/main/java/org/wendex/StoreActor.java
@@ -7,13 +7,15 @@ import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
 
 public class StoreActor extends AbstractActor {
-    private HashMap<Pair<String, Integer>, Long> store;
+    private HashMap<Pair<String, Integer>, Long> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(ResultMessage.class, m -> {
-                    store.put(m.)
+                    store.put(m.getPair(), m.getTime());
+                }).match(QueryMessage.class, m -> {
+
                 })
                 .build();
     }
