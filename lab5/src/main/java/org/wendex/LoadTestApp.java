f8e3e769174c26cac5966fd9448e6a367fd4bb4f
Wendex <2wendex2@mail.ru>
2020-12-20 18:44:37 +0300

    Scripted auto-commit on change (2020-12-20 18:44:37) by gitwatch.sh

--- lab5/src/main/java/org/wendex/LoadTestApp.java
+++ lab5/src/main/java/org/wendex/LoadTestApp.java
@@ -20,7 +20,7 @@ public class LoadTestApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(HttpRequest.class).map(x -> {
-
+            
         });
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
