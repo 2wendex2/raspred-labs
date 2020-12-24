af055d5b0273befbd1aea3e8744d58e0884460b1
Wendex <2wendex2@mail.ru>
2020-12-24 09:25:36 +0300

    Scripted auto-commit on change (2020-12-24 09:25:36) by gitwatch.sh

--- lab6/src/main/java/org/wendex/ZooActor.java
+++ lab6/src/main/java/org/wendex/ZooActor.java
@@ -3,5 +3,10 @@ package org.wendex;
 import akka.actor.AbstractActor;
 
 public class ZooActor extends AbstractActor {
-    
+    private int[] ports;
+
+    @Override
+    public Receive createReceive() {
+        return ;
+    }
 }
