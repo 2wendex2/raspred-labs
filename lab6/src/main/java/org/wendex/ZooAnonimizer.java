0780847080f3f38c112807beab2ee18bed68dcd7
Wendex <2wendex2@mail.ru>
2020-12-24 07:51:20 +0300

    Scripted auto-commit on change (2020-12-24 07:51:16) by gitwatch.sh

--- lab6/src/main/java/org/wendex/ZooAnonimizer.java
+++ lab6/src/main/java/org/wendex/ZooAnonimizer.java
@@ -14,7 +14,7 @@ public class ZooAnonimizer implements Watcher {
 
     public ZooAnonimizer() throws Exception {
         zoo = new ZooKeeper("127.0.0.1:2181", 3000, this);
-        zoo.create("/servers/s", "data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL_SEQUENTIAL);
+        //zoo.create("/servers/s", "data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL_SEQUENTIAL);
         List<String> servers = zoo.getChildren("/servers", this);
         for (String s : servers) {
             byte[] data = zoo.getData("/servers/" + s, false, null);
