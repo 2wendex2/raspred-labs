a790a11f08e2b99e41767c12b47680ef1c677e42
Wendex <2wendex2@mail.ru>
2020-12-24 09:20:35 +0300

    Scripted auto-commit on change (2020-12-24 09:20:34) by gitwatch.sh

--- lab6/src/main/java/org/wendex/ServerListMessage.java
+++ lab6/src/main/java/org/wendex/ServerListMessage.java
@@ -1,4 +1,13 @@
 package org.wendex;
 
 public class ServerListMessage {
+    private int[] ports;
+
+    public int[] getPorts() {
+        return ports;
+    }
+
+    public ServerListMessage(int[] ports) {
+        this.ports = ports;
+    }
 }
