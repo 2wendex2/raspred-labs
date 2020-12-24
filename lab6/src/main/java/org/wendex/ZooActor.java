1b34b81232657e5a72143ba3f1bbfd2f1d9d88bf
Wendex <2wendex2@mail.ru>
2020-12-24 09:37:44 +0300

    Scripted auto-commit on change (2020-12-24 09:37:44) by gitwatch.sh

--- lab6/src/main/java/org/wendex/ZooActor.java
+++ lab6/src/main/java/org/wendex/ZooActor.java
@@ -3,13 +3,19 @@ package org.wendex;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.Random;
+
 public class ZooActor extends AbstractActor {
-    private int[] ports;
+    private int[] ports = new int[0];
+    private Random random = new Random();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(ServerListMessage.class, m -> ports = m.getPorts())
-                .match(ServerQueryMessage.class, )
+                .match(ServerQueryMessage.class, m -> {
+                    int index = random.nextInt(ports.length);
+
+                })
     }
 }
