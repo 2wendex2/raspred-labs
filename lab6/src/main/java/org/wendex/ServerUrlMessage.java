9b530ae0c79f125fd5ba898dfbfce5f72a286dc6
Wendex <2wendex2@mail.ru>
2020-12-24 09:21:31 +0300

    Scripted auto-commit on change (2020-12-24 09:21:31) by gitwatch.sh

--- lab6/src/main/java/org/wendex/ServerUrlMessage.java
+++ lab6/src/main/java/org/wendex/ServerUrlMessage.java
@@ -1,4 +1,13 @@
 package org.wendex;
 
 public class ServerUrlMessage {
+    private String[] url;
+
+    public String[] getUrl() {
+        return url;
+    }
+
+    public ServerUrlMessage(String[] url) {
+        this.url = url;
+    }
 }
