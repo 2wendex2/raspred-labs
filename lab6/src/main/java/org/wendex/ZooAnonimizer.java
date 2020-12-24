ebe28affc3b2f14bf2c55462c8ccaf80b6123034
Wendex <2wendex2@mail.ru>
2020-12-24 07:15:24 +0300

    Scripted auto-commit on change (2020-12-24 07:15:24) by gitwatch.sh

--- lab6/src/main/java/org/wendex/ZooAnonimizer.java
+++ lab6/src/main/java/org/wendex/ZooAnonimizer.java
@@ -1,17 +1,18 @@
 package org.wendex;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+
+import java.util.List;
 
 public class ZooAnonimizer {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, new DummyWatcher());
-        zoo.create("/servers/s",
-                “data”.getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
-                CreateMode.EPHEMERAL_SEQUENTIAL
-);
+        zoo.create("/servers/s", "data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL_SEQUENTIAL);
         List<String> servers = zoo.getChildren("/servers", this);
         for (String s : servers) {
             byte[] data = zoo.getData("/servers/" + s, false, null);
