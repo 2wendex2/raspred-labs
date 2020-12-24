f3258a01e7a6e20967cf5b478da4b1f6b13b595a
Wendex <2wendex2@mail.ru>
2020-12-24 07:19:35 +0300

    Scripted auto-commit on change (2020-12-24 07:19:35) by gitwatch.sh

--- lab6/src/main/java/org/wendex/AnonimizerWatcher.java
+++ lab6/src/main/java/org/wendex/AnonimizerWatcher.java
@@ -1,4 +1,11 @@
 package org.wendex;
 
-public class AnonimizerWatcher {
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class AnonimizerWatcher extends Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
