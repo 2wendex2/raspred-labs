01ce6116accf45711ada7359973e7d4bc71ad060
Wendex <2wendex2@mail.ru>
2020-12-25 12:39:15 +0300

    Scripted auto-commit on change (2020-12-25 12:39:14) by gitwatch.sh

--- lab7/client/src/main/java/org/wendex/CmdHandler.java
+++ lab7/client/src/main/java/org/wendex/CmdHandler.java
@@ -42,9 +42,6 @@ public class CmdHandler {
     }
 
     public static void main(String[] args) {
-        if (args.length != 1)
-            throw new IllegalArgumentException("Wrong arguments count");
-
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket requester = context.socket(SocketType.REQ);
         requester.connect(PROXY_URL);
