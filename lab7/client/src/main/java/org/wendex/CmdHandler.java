b779b77cfe81d70301ce293f36b394cbfc9667cb
Wendex <2wendex2@mail.ru>
2020-12-25 10:22:27 +0300

    Scripted auto-commit on change (2020-12-25 10:22:27) by gitwatch.sh

--- lab7/client/src/main/java/org/wendex/CmdHandler.java
+++ lab7/client/src/main/java/org/wendex/CmdHandler.java
@@ -6,6 +6,8 @@ import org.zeromq.ZMQ;
 public class CmdHandler {
     static String URL_HEAD = "tcp://";
 
+    private 
+
     public static void main(String[] args) {
         if (args.length != 1)
             throw new IllegalArgumentException("Wrong arguments count");
@@ -14,6 +16,6 @@ public class CmdHandler {
         ZMQ.Socket requester = context.socket(SocketType.REQ);
         requester.connect(URL_HEAD + args[0]);
         System.out.println("Launch and connect client");
-        
+
     }
 }
