b807dbc8c62c90fb76165e0d8b1ddaa84cbfbb00
Wendex <2wendex2@mail.ru>
2020-12-25 12:40:01 +0300

    Scripted auto-commit on change (2020-12-25 12:40:00) by gitwatch.sh

--- lab7/proxy/src/main/java/org/wendex/CacheProxy.java
+++ lab7/proxy/src/main/java/org/wendex/CacheProxy.java
@@ -3,6 +3,22 @@ package org.wendex;
 public class CacheProxy {
 
     public static void main(String[] args) {
-
+        Context context = ZMQ.context(1);
+        Socket frontend =
+                context.socket(SocketType.ROUTER);
+        Socket backend =
+                context.socket(SocketType.DEALER);
+        frontend.bind("tcp://*:5559");
+        backend.bind("tcp://*:5560");
+        System.out.println("launch and connect
+                broker.");
+                Poller items = context.poller (2);
+        items.register(frontend, Poller.POLLIN);
+        items.register(backend, Poller.POLLIN);
+        boolean more = false;
+        byte[] message;
+        while (!Thread.currentThread().isInterrupted()) {
+            items.poll();
+            
     }
 }
