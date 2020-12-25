c02254c8c6230a0b1d2e585a2941c87c785c5951
Wendex <2wendex2@mail.ru>
2020-12-25 16:20:09 +0300

    Scripted auto-commit on change (2020-12-25 16:20:09) by gitwatch.sh

--- lab7/storage/src/main/java/org/wendex/CacheStorage.java
+++ lab7/storage/src/main/java/org/wendex/CacheStorage.java
@@ -1,6 +1,11 @@
 package org.wendex;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+
 public class CacheStorage {
+    private static final String PROXY_URL = "tcp://127.0.0.1:5560";
+
     private int beginInterval;
     private int[] interval;
 
@@ -37,6 +42,43 @@ public class CacheStorage {
 
     public static void main(String[] args) {
         CacheStorage storage = fromStrings(args);
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
+        frontend.bind(FRONT_URL);
+        backend.bind(BACK_URL);
+        System.out.println("launch and connect broker");
+        ZMQ.Poller items = context.poller (2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
+        boolean more = false;
+        byte[] message;
 
+        while (!Thread.currentThread().isInterrupted()) {
+            items.poll(1000);
+            items.pollin(FRONT_INDEX);
+            if (items.pollin(FRONT_INDEX)) {
+                do {
+                    message = frontend.recv(0);
+                    more = frontend.hasReceiveMore();
+                    if (more)
+                        frontend.send(message, ZMQ.SNDMORE);
+                    else
+                        frontend.send(BytesTools.intToBytes(4), 0);
+                } while (more);
+            }
+            if (items.pollin(1)) {
+                do {
+                    message = backend.recv(0);
+                    more = backend.hasReceiveMore();
+                    frontend.send(message, more ? ZMQ.SNDMORE : 0);
+                } while (more);
+            }
+        }
+
+        items.close();
+        backend.close();
+        frontend.close();
+        context.term();
     }
 }
