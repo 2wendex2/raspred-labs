208cf47a508dd2beaa2d21347daf756fe25d2b69
Wendex <2wendex2@mail.ru>
2020-12-25 10:15:10 +0300

    Scripted auto-commit on change (2020-12-25 10:15:09) by gitwatch.sh

--- lab7/client/src/main/java/org/wendex/CmdHandler.java
+++ lab7/client/src/main/java/org/wendex/CmdHandler.java
@@ -4,6 +4,9 @@ public class CmdHandler {
 
 
     public static void main(String[] args) {
+        if (args.length != 1)
+            throw new IllegalArgumentException("Wrong arguments count");
+
         
     }
 }
