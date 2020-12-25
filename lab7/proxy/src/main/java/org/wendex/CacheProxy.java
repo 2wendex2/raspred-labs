bd918c0a01091d0c3d85f7c5eef676cefaf33cf0
Wendex <2wendex2@mail.ru>
2020-12-25 12:52:11 +0300

    Scripted auto-commit on change (2020-12-25 12:52:02) by gitwatch.sh

--- lab7/proxy/src/main/java/org/wendex/CacheProxy.java
+++ lab7/proxy/src/main/java/org/wendex/CacheProxy.java
@@ -1,45 +1,45 @@
 package org.wendex;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+
 public class CacheProxy {
     private static final String FRONT_URL = "tcp://*:5559";
     private static final String BACK_URL = "tcp://*:5560";
 
+    private static int FRONT_INDEX = 0;
+    private static int BACK_INDEX = 1;
+
     public static void main(String[] args) {
-        Context context = ZMQ.context(1);
-        Socket frontend =
-                context.socket(SocketType.ROUTER);
-        Socket backend =
-                context.socket(SocketType.DEALER);
-        frontend.bind("tcp://*:5559");
-        backend.bind("tcp://*:5560");
-        System.out.println("launch and connect
-                broker.");
-                Poller items = context.poller (2);
-        items.register(frontend, Poller.POLLIN);
-        items.register(backend, Poller.POLLIN);
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
+        frontend.bind(FRONT_URL);
+        backend.bind(BACK_URL);
+        System.out.println("launch and connect broker");
+        ZMQ.Poller items = context.poller (2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
         boolean more = false;
         byte[] message;
         while (!Thread.currentThread().isInterrupted()) {
             items.poll();
-            if (items.pollin(0)) {
-                while (true) {
-                    message = frontend.recv(0);
-                    more = frontend.hasReceiveMore();
-                    backend.send(message, more ? ZMQ.SNDMORE : 0);
-                    if(!more){
-                        break;
-                    }
-                }
+            if (items.pollin(FRONT_INDEX)) {
+                do {
+                    DataRequest request = DataRequest.fromBytes(frontend.recv());
+                    more = backend.hasReceiveMore();
+                    if (request instanceof GetRequest)
+                        frontend.send(BytesTools.intToBytes(4));
+                    else
+                        frontend.send(BytesTools.boolToBytes(true));
+                } while (more);
             }
             if (items.pollin(1)) {
-                while (true) {
+                do {
                     message = backend.recv(0);
                     more = backend.hasReceiveMore();
                     frontend.send(message, more ? ZMQ.SNDMORE : 0);
-                    if(!more){
-                        break;
-                    }
-                }
+                } while (more);
             }
         }
     }
