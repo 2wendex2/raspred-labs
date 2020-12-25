33a01fc3c415629eb3109c350166f7ae1cf4cc70
Wendex <2wendex2@mail.ru>
2020-12-25 09:40:57 +0300

    Scripted auto-commit on change (2020-12-25 09:40:56) by gitwatch.sh

--- lab7/src/main/java/org/wendex/CacheStorage.java
+++ lab7/src/main/java/org/wendex/CacheStorage.java
@@ -1,4 +1,18 @@
 package org.wendex;
 
 public class CacheStorage {
+    private int intervalBegin;
+    private int intervalEnd;
+
+    public int getIntervalBegin() {
+        return intervalBegin;
+    }
+
+    public int getIntervalEnd() {
+        return intervalEnd;
+    }
+
+    public int getLength() {
+        return intervalEnd - intervalEnd + 1;
+    }
 }
