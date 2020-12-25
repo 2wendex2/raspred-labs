1eccd12fbe8765773b909733b020337418603d38
Wendex <2wendex2@mail.ru>
2020-12-25 15:02:56 +0300

    Scripted auto-commit on change (2020-12-25 15:02:56) by gitwatch.sh

--- lab7/proxy/src/main/java/org/wendex/CacheProxy.java
+++ lab7/proxy/src/main/java/org/wendex/CacheProxy.java
@@ -25,6 +25,12 @@ public class CacheProxy {
 
         message = frontend.recv(0);
         System.out.println(message.length);
+        System.out.println(message[0]);
+        System.out.println(message[1]);
+        System.out.println(message[2]);
+        System.out.println(message[3]);
+        System.out.println(message[4]);
+
 
         /*while (!Thread.currentThread().isInterrupted()) {
             items.poll(1000);
