7f68c6b10643ed94d33a13ff42e491458ad32ba2
Wendex <2wendex2@mail.ru>
2020-12-25 11:41:45 +0300

    Scripted auto-commit on change (2020-12-25 11:41:45) by gitwatch.sh

--- lab7/src/main/java/org/wendex/BytesTools.java
+++ lab7/src/main/java/org/wendex/BytesTools.java
@@ -1,4 +1,29 @@
 package org.wendex;
 
 public class BytesTools {
+    static public byte[] intToBytes(int v) {
+        byte[] bytes = new byte[8];
+        bytes[0] = (byte)v;
+        bytes[1] = (byte)(v >>> 8);
+        bytes[2] = (byte)(v >>> 16);
+        bytes[3] = (byte)(v >>> 24);
+        return bytes;
+    }
+
+    static public int bytesToInt(byte[] bytes){
+        return (bytes[0] & 0xFF) | ((bytes[1] & 0xFF) << 8) | ((bytes[2] & 0xFF) << 16) | ((bytes[3] & 0xFF) << 24);
+    }
+
+    static public byte[] boolToBytes(boolean b) {
+        byte[] bytes = new byte[1];
+        if (b)
+            bytes[0] = 1;
+        else
+            bytes[0] = 0;
+        return bytes;
+    }
+
+    static public boolean bytesToBool(byte[] bytes) {
+        return bytes[0] != 0;
+    }
 }
