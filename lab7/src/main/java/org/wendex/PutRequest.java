00f0b2d9af35500c47ab2076083415a9a3d08326
Wendex <2wendex2@mail.ru>
2020-12-25 11:03:32 +0300

    Scripted auto-commit on change (2020-12-25 11:03:29) by gitwatch.sh

--- lab7/src/main/java/org/wendex/PutRequest.java
+++ lab7/src/main/java/org/wendex/PutRequest.java
@@ -11,4 +11,23 @@ public class PutRequest extends DataRequest {
     public int getValue() {
         return value;
     }
+
+    public byte[] toBytes() {
+        byte[] bytes = new byte[8];
+        bytes[0] = (byte)cell;
+        bytes[1] = (byte)(cell >>> 8);
+        bytes[2] = (byte)(cell >>> 16);
+        bytes[3] = (byte)(cell >>> 24);
+
+        bytes[4] = (byte)value;
+        bytes[5] = (byte)(value >>> 8);
+        bytes[6] = (byte)(value >>> 16);
+        bytes[7] = (byte)(value >>> 24);
+        return bytes;
+    }
+
+    public PutRequest(byte[] bytes) {
+        cell = (bytes[0] & 0xFF) | ((bytes[1] & 0xFF) << 8) | ((bytes[2] & 0xFF) << 16) | ((bytes[3] & 0xFF) << 24);
+        value = (bytes[4] & 0xFF) | ((bytes[5] & 0xFF) << 8) | ((bytes[6] & 0xFF) << 16) | ((bytes[7] & 0xFF) << 24);
+    }
 }
