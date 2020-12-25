59b4b0c5c82cf5358421736d81c5a7cf5b0a3bcf
Wendex <2wendex2@mail.ru>
2020-12-25 11:03:53 +0300

    Scripted auto-commit on change (2020-12-25 11:03:53) by gitwatch.sh

--- lab7/src/main/java/org/wendex/GetRequest.java
+++ lab7/src/main/java/org/wendex/GetRequest.java
@@ -4,4 +4,17 @@ public class GetRequest extends DataRequest {
     public GetRequest(int cell) {
         this.cell = cell;
     }
+
+    public byte[] toBytes() {
+        byte[] bytes = new byte[8];
+        bytes[0] = (byte)cell;
+        bytes[1] = (byte)(cell >>> 8);
+        bytes[2] = (byte)(cell >>> 16);
+        bytes[3] = (byte)(cell >>> 24);
+        return bytes;
+    }
+
+    public PutRequest(byte[] bytes) {
+        cell = (bytes[0] & 0xFF) | ((bytes[1] & 0xFF) << 8) | ((bytes[2] & 0xFF) << 16) | ((bytes[3] & 0xFF) << 24);
+    }
 }
