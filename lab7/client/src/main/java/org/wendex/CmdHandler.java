369a136501d9a6f6582ffd1dfe2392ea8382dde0
Wendex <2wendex2@mail.ru>
2020-12-25 10:21:14 +0300

    Scripted auto-commit on change (2020-12-25 10:21:13) by gitwatch.sh

--- lab7/client/src/main/java/org/wendex/CmdHandler.java
+++ lab7/client/src/main/java/org/wendex/CmdHandler.java
@@ -13,6 +13,6 @@ public class CmdHandler {
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket requester = context.socket(SocketType.REQ);
         requester.connect(URL_HEAD + args[0]);
-        
+        System.out.printl("Launch and connect client");
     }
 }
