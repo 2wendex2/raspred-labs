b35e3b363a93c1db81f5e5706eec3f096ba301f4
Wendex <2wendex2@mail.ru>
2020-12-25 17:56:14 +0300

    Scripted auto-commit on change (2020-12-25 17:56:14) by gitwatch.sh

--- lab7/proxy/src/main/java/org/wendex/Storage.java
+++ lab7/proxy/src/main/java/org/wendex/Storage.java
@@ -1,4 +1,25 @@
 package org.wendex;
 
 public class Storage {
+    private byte[] routerFrame;
+    private int beginInterval;
+    private int endInterval;
+
+    public int getBeginInterval() {
+        return beginInterval;
+    }
+
+    public byte[] getRouterFrame() {
+        return routerFrame;
+    }
+
+    public int getEndInterval() {
+        return endInterval;
+    }
+
+    public Storage(byte[] routerFrame, int beginInterval, int endInterval) {
+        this.routerFrame = routerFrame;
+        this.beginInterval = beginInterval;
+        this.endInterval = endInterval;
+    }
 }
